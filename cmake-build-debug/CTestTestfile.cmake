5d77f76a60a89f1ed45b1a6f071faf910b9aca02
Jollu8 <zholu8@gmail.com>
2022-11-01 20:01:51 +0300

    changed rebind

---
+++ cmake-build-debug/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /Users/jolluemil/Desktop/mapallocatorv2
+# Build directory: /Users/jolluemil/Desktop/mapallocatorv2/cmake-build-debug
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("src")
+subdirs("test")
