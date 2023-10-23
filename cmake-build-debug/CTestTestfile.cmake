e17a9e23e7c4fd7c406d83be06e2b292297e42df
Jollu <zholu8@gmail.com>
2023-10-23 13:45:40 +0300

    Delete cmake-build-debug directory

--- cmake-build-debug/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/jolluemil/Desktop/mapallocatorv2
-# Build directory: /Users/jolluemil/Desktop/mapallocatorv2/cmake-build-debug
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("src")
-subdirs("test")
--- cmake-build-debug/test/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/jolluemil/Desktop/mapallocatorv2/test
-# Build directory: /Users/jolluemil/Desktop/mapallocatorv2/cmake-build-debug/test
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(gtest_allocator "gtest_allocator")
-set_tests_properties(gtest_allocator PROPERTIES  _BACKTRACE_TRIPLES "/Users/jolluemil/Desktop/mapallocatorv2/test/CMakeLists.txt;42;add_test;/Users/jolluemil/Desktop/mapallocatorv2/test/CMakeLists.txt;0;")
